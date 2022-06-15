ac1a6e7a7b94945d13fd2cfd2fc04f70acc093fb
Pedro L Freitas <pllsatierf@gmail.com>
2022-06-15 00:51:50 -0300

    changing nothing
    changing nothing

--- app/src/test/java/com/plfdev/unitest/ExampleUnitTest.kt
+++ app/src/test/java/com/plfdev/unitest/ExampleUnitTest.kt
@@ -3,11 +3,6 @@ package com.plfdev.unitest
 import org.junit.Test
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
